8daaac2874fe4ae076170da74324729c77f217d1
AlekandarAleksicEngIt <aleksandar.aleksic@eng.it>
2021-07-21 11:07:44 +0200

    Training day 31

---
+++ Swift/Day31/SpacePhoto/SpacePhoto/URL+Helpers.swift
@@ -0,0 +1,26 @@
+//
+//  URL+Helpers.swift
+//  SpacePhoto
+//
+//  Created by aleksandar.aleksic on 6/8/21.
+//
+
+import Foundation
+
+extension URL{
+    func withQueries(_ queries: [String: String]) -> URL?{
+        var components = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        components!.queryItems = queries.map{
+            URLQueryItem(name: $0.0, value: $0.1)
+        }
+        return components!.url
+    }
+    
+    func withHTTPS() -> URL!{
+        var component = URLComponents(url: self, resolvingAgainstBaseURL: true)
+        component?.scheme = "https"
+        return component?.url
+    }
+}
+
+
