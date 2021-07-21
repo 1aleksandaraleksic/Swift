b8951d55bbd8cbda43010290740a1dcf03d557bc
AlekandarAleksicEngIt <aleksandar.aleksic@eng.it>
2021-07-21 17:34:42 +0200

    init commit, created project MessangerChat

---
+++ Swift/ChatApp/MessangerChat/MessangerChat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MessangerChat
+//
+//  Created by aleksandar.aleksic on 21.7.21..
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
